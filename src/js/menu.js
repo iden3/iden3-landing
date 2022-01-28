936a590e34d5371d1d0320bdca130bf620b10008
Alex Monso <amonsosanz@gmail.com>
2022-01-28 20:03:16 +0100

    Implement mobile menu

---
+++ src/js/menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+  const menu = document.getElementById("mobile-menu");
+  menu.style.width = "100%";
+}
+
+function closeNav() {
+  const menu = document.getElementById("mobile-menu");
+  menu.style.width = 0;
+}
